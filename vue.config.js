dd1fd2cc94b910ea37f5b4d8805300ad447ad09a
Mike Rodrigues <rodrigues.mi@husky.neu.edu>
2019-02-17 01:43:44 -0500

    first commit

---
+++ vue.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  assetsDir: 'assets',
+  publicPath: undefined,
+  outputDir: undefined,
+  runtimeCompiler: undefined,
+  productionSourceMap: false,
+  parallel: undefined,
+  css: undefined,
+  lintOnSave: undefined
+}
